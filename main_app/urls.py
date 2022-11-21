73249bd7ec8a5b653a7958c11fdefa58fedbbe40
Kgchek <rptr22f@msn.com>
2022-11-21 16:09:28 -0700

    API test

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('posts/', views.Search.as_view(), name='search')
     
 ]
