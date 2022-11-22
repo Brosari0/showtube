987529263c79f8812ac867d093eda75bf85157a7
brosari0 <Trumpetskickbrass86@gmail.com>
2022-11-22 00:57:43 +0000

    add model, search query page, env file, home search, urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
-    
+    path('posts/search/', views.search, name='search'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,10 @@
+import requests
+import json, os
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
+DEVELOPER_KEY = os.environ['DEVELOPER_KEY']
 # Create your views here.
 def home(request):
     return render(request,'home.html')
@@ -20,3 +23,10 @@ def signup(request):
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)       
     
+def search(request):
+    print(DEVELOPER_KEY)
+    query = request.GET.get('search')
+    search_url = 'https://www.googleapis.com/youtube/v3/search'
+    response = requests.get(f"{search_url}?query={query}&key={DEVELOPER_KEY}").json()
+    print(response)
+    return render(request, 'posts/search.html', {'videos': response})
