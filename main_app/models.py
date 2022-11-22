c4ad0391da42b85efdd7ecf20414c9541e01d2d3
Will <willjian97@gmail.com>
2022-11-22 11:55:16 -0800

    refactor search and create and update model to post

--- main_app/models.py
+++ main_app/models.py
@@ -1,28 +1,18 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
-class Video(models.Model):
+class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     video_id = models.CharField(max_length=255, unique=True, null=True,
                                 help_text=("The Youtube id of the video"))
-    title = models.CharField(max_length=200, null=True, blank=True)
+    title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    keywords = models.CharField(max_length=200, null=True, blank=True,
-                                help_text=("Comma seperated keywords"))
     youtube_url = models.URLField(max_length=255, null=True, blank=True)
-
+    comment = models.CharField(max_length=100, null=True, blank=True)
 
     def get_absolute_url(self):
-        return self.url
+        return reverse('posts_detail', kwargs={'pk':self.id})
 
-# class Thumbnail(models.Model):
-#     video = models.ForeignKey(Video, null=True)
-#     url = models.URLField(max_length=255)
-
-#     def __unicode__(self):
-#         return self.url
-
-#     def get_absolute_url(self):
-#         return self.url
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from django.urls import re_path
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
     path('posts/search/', views.search, name='search'),
+  
+    path('posts/create/<str:video_id>', views.Create.as_view(), name='create'),
+    #re_path(r'posts/create/(?P<post_id>[0-9]+)/$', views.Create.as_view(), name='create'),
+
+
 ]
